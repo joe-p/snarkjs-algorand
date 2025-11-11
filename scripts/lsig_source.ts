8c64d7857bbe370c52c2a7ebafbb11a5e9107e15
Joe Polny <joepolny@gmail.com>
2025-11-11 12:40:20 -0500

    chore: use tsx for scripts

--- scripts/lsig_source.ts
+++ scripts/lsig_source.ts
@@ -1,10 +1,19 @@
 import { readFileSync, writeFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const lsigTeal = readFileSync(
-  __dirname + "/../contracts/out/PlonkVerifierLsig.teal",
+  join(__dirname, "..", "contracts", "out", "PlonkVerifierLsig.teal"),
   "utf8",
 );
 
 const content = `export const LSIG_SOURCE = \`${lsigTeal}\`;\n`;
 
-writeFileSync(__dirname + "/../contracts/out/lsig_source.ts", content);
+writeFileSync(
+  join(__dirname, "..", "contracts", "out", "lsig_source.ts"),
+  content,
+);
+console.log("Generated lsig_source.ts");
