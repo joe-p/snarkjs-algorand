8f22dd1cb4697742cc108f50077c5799ac4680f1
Joe Polny <joepolny@gmail.com>
2025-11-16 14:08:27 -0500

    chore: abstract verifier classes

--- src/groth16.ts
+++ src/groth16.ts
@@ -1,4 +1,4 @@
-import { AlgorandClient, microAlgos } from "@algorandfoundation/algokit-utils";
+import { AlgorandClient } from "@algorandfoundation/algokit-utils";
 import {
   Groth16VerifierClient,
   Groth16VerifierFactory,
@@ -13,14 +13,15 @@ import {
   getABIEncodedValue,
   type Arc56Contract,
 } from "@algorandfoundation/algokit-utils/types/app-arc56";
-import { readFileSync } from "fs";
-import type { RawSimulateOptions } from "@algorandfoundation/algokit-utils/types/composer";
-import type { Transaction } from "algosdk";
-import type { AppClientMethodCallParams } from "@algorandfoundation/algokit-utils/types/app-client";
 import type { Address } from "algosdk";
 import { GROTH16_LSIG_SOURCE } from "../contracts/out/lsig_source";
-import type { AlgoAmount } from "@algorandfoundation/algokit-utils/types/amount";
 import { stringValuesToBigints } from "./index.ts";
+import {
+  AppVerifier,
+  LsigVerifier,
+  reorderG2Uncompressed,
+  getProofFromFile,
+} from "./common";
 
 export {
   Groth16SignalsAndProofFactory,
@@ -61,18 +62,7 @@ export async function getGroth16Vkey(
     stringValuesToBigints(vkey[pointName]);
     const point = curve.G2.fromObject(vkey[pointName]);
     const uncompressed = curve.G2.toUncompressed(point);
-
-    // Reorder G2 point components from [x1, x0, y1, y0] to [x0, x1, y0, y1]
-    const x1 = uncompressed.subarray(0, 48);
-    const x0 = uncompressed.subarray(48, 96);
-    const y1 = uncompressed.subarray(96, 144);
-    const y0 = uncompressed.subarray(144, 192);
-
-    const reordered = new Uint8Array(192);
-    reordered.set(x0, 0);
-    reordered.set(x1, 48);
-    reordered.set(y0, 96);
-    reordered.set(y1, 144);
+    const reordered = reorderG2Uncompressed(uncompressed);
 
     g2Bytes[pointName] = reordered;
   }
@@ -98,7 +88,7 @@ export async function getGroth16Proof(
   path: string,
   curve: any,
 ): Promise<GrothProof> {
-  const proof = JSON.parse(readFileSync(path, "utf8"));
+  const proof = getProofFromFile(path);
   return encodeGroth16Proof(proof, curve);
 }
 
@@ -114,18 +104,7 @@ export function encodeGroth16Proof(proof: any, curve: any): GrothProof {
   stringValuesToBigints(proof.pi_b);
   const piBPoint = curve.G2.fromObject(proof.pi_b);
   const piBUncompressed = curve.G2.toUncompressed(piBPoint);
-
-  // Reorder G2 point components from [x1, x0, y1, y0] to [x0, x1, y0, y1]
-  const x1 = piBUncompressed.subarray(0, 48);
-  const x0 = piBUncompressed.subarray(48, 96);
-  const y1 = piBUncompressed.subarray(96, 144);
-  const y0 = piBUncompressed.subarray(144, 192);
-
-  const piBBytes = new Uint8Array(192);
-  piBBytes.set(x0, 0);
-  piBBytes.set(x1, 48);
-  piBBytes.set(y0, 96);
-  piBBytes.set(y1, 144);
+  const piBBytes = reorderG2Uncompressed(piBUncompressed);
 
   return {
     piA: proof.pi_aBytes,
@@ -134,277 +113,95 @@ export function encodeGroth16Proof(proof: any, curve: any): GrothProof {
   };
 }
 
-export function encodeGroth16Signals(...inputs: string[]) {
-  return inputs.map((input) => {
-    return BigInt(input);
-  });
-}
-
 export type Groth16Witness = {
   proof: GrothProof;
   signals: bigint[];
 };
 
-export class Groth16LsigVerifier {
-  curve?: any;
-
-  constructor(
-    public algorand: AlgorandClient,
-    public zKey: snarkjs.ZKArtifact,
-    public wasmProver: snarkjs.ZKArtifact,
-    public totalLsigs: number = 6,
-  ) {}
-
-  private async ensureCurveInstanttiation() {
-    if (!this.curve) {
-      // @ts-expect-error curves is not typed
-      this.curve = await snarkjs.curves.getCurveFromName("bls12381");
-    }
+export class Groth16LsigVerifier extends LsigVerifier<
+  GrothVerificationKey,
+  Groth16Witness
+> {
+  protected async getVkey(
+    zKey: snarkjs.ZKArtifact,
+    curve: any,
+  ): Promise<GrothVerificationKey> {
+    return getGroth16Vkey(zKey, curve);
   }
 
-  async proofAndSignals(
+  protected encodeVkey(
+    vk: GrothVerificationKey,
+    appSpec: Arc56Contract,
+  ): Uint8Array {
+    return encodeGroth16Vk(vk, appSpec);
+  }
+
+  protected encodeProof(proof: any, curve: any): GrothProof {
+    return encodeGroth16Proof(proof, curve);
+  }
+
+  protected async fullProve(
     inputs: snarkjs.CircuitSignals,
-  ): Promise<Groth16Witness> {
-    await this.ensureCurveInstanttiation();
-
-    const { proof: rawProof, publicSignals: rawSignals } =
-      await snarkjs.groth16.fullProve(inputs, this.wasmProver, this.zKey);
-
-    const proof = encodeGroth16Proof(rawProof, this.curve);
-    const signals = encodeGroth16Signals(...rawSignals);
-
-    return {
-      proof,
-      signals,
-    };
+    wasmProver: snarkjs.ZKArtifact,
+    zKey: snarkjs.ZKArtifact,
+  ): Promise<{ proof: any; publicSignals: any }> {
+    return snarkjs.groth16.fullProve(inputs, wasmProver, zKey);
   }
 
-  async lsigAccount() {
-    await this.ensureCurveInstanttiation();
-
-    const vk = await getGroth16Vkey(this.zKey, this.curve);
-    const vkBytes = encodeGroth16Vk(vk, APP_SPEC);
-
-    const compilation = await this.algorand.app.compileTealTemplate(
-      GROTH16_LSIG_SOURCE,
-      {
-        VERIFICATION_KEY: vkBytes,
-      },
-    );
-
-    return this.algorand.account.logicsig(compilation.compiledBase64ToBytes);
+  protected getLsigSource(): string {
+    return GROTH16_LSIG_SOURCE;
   }
 
-  async verificationParams({
-    inputs,
-    composer,
-    paramsCallback,
-    addExtraLsigs = true,
-  }: {
-    inputs: snarkjs.CircuitSignals;
-    composer: {
-      addTransaction: (txn: Transaction) => unknown;
-    };
-    addExtraLsigs?: boolean;
-    paramsCallback: (params: {
-      appParams: {
-        sender: Address;
-        staticFee: AlgoAmount;
-        args: { signals: bigint[]; proof: GrothProof };
-      };
-      lsigsFee: AlgoAmount;
-      extraLsigsTxns: Transaction[];
-    }) => Promise<void>;
-  }): Promise<void> {
-    const { proof, signals } = await this.proofAndSignals(inputs);
-
-    const params = {
-      appParams: {
-        sender: await this.lsigAccount(),
-        staticFee: microAlgos(0),
-        args: { signals, proof },
-      },
-      lsigsFee: microAlgos(1000 * this.totalLsigs),
-      extraLsigsTxns: [] as Transaction[],
-    };
-
-    const compilation = await this.algorand.app.compileTeal(
-      "#pragma version 11\n txn RekeyTo; global ZeroAddress; ==",
-    );
-    const extraLsig = this.algorand.account.logicsig(
-      compilation.compiledBase64ToBytes,
-    );
-
-    await paramsCallback(params);
-
-    for (let i = 0; i < this.totalLsigs - 1; i++) {
-      const lsigPay = await this.algorand.createTransaction.payment({
-        sender: extraLsig,
-        amount: microAlgos(0),
-        staticFee: microAlgos(0),
-        receiver: extraLsig,
-        note: `Extra lsig ${i + 1} of ${this.totalLsigs - 1}`,
-      });
-
-      params.extraLsigsTxns.push(lsigPay);
-
-      if (addExtraLsigs) {
-        composer.addTransaction(lsigPay);
-      }
-    }
+  protected getAppSpec(): Arc56Contract {
+    return APP_SPEC;
   }
 }
 
-export class Groth16AppVerifier {
-  appClient?: Groth16VerifierClient;
-  curve?: any;
-
-  constructor(
-    public algorand: AlgorandClient,
-    public zKey: snarkjs.ZKArtifact,
-    public wasmProver: snarkjs.ZKArtifact,
-  ) {}
-
-  private async ensureCurveInstanttiation() {
-    if (!this.curve) {
-      // @ts-expect-error curves is not typed
-      this.curve = await snarkjs.curves.getCurveFromName("bls12381");
-    }
+export class Groth16AppVerifier extends AppVerifier<
+  Groth16VerifierFactory,
+  Groth16VerifierWithLogsFactory,
+  Groth16VerifierClient,
+  Groth16Witness,
+  Groth16VerifierDeployParams,
+  GrothVerificationKey
+> {
+  protected newFactory(o: {
+    algorand: AlgorandClient;
+    defaultSender: Address;
+  }): Groth16VerifierFactory {
+    return new Groth16VerifierFactory(o);
   }
 
-  async deploy(
-    params: Omit<Groth16VerifierDeployParams, "deployTimeParams"> & {
-      defaultSender: Address;
-      debugLogging?: boolean;
-    },
-  ) {
-    if (this.appClient) {
-      throw new Error("AppVerifier already deployed");
-    }
-
-    await this.ensureCurveInstanttiation();
-
-    let factory;
-
-    if (params.debugLogging) {
-      factory = new Groth16VerifierWithLogsFactory({
-        algorand: this.algorand,
-        defaultSender: params.defaultSender,
-      });
-    } else {
-      factory = new Groth16VerifierFactory({
-        algorand: this.algorand,
-        defaultSender: params.defaultSender,
-      });
-    }
-
-    const vk = await getGroth16Vkey(this.zKey, this.curve);
-    const vkBytes = encodeGroth16Vk(vk, factory.appSpec);
-
-    const { appClient } = await factory.deploy({
-      ...params,
-      deployTimeParams: {
-        VERIFICATION_KEY: vkBytes,
-      },
-    });
-
-    this.appClient = appClient;
-    return appClient;
+  protected newLogsFactory(o: {
+    algorand: AlgorandClient;
+    defaultSender: Address;
+  }): Groth16VerifierWithLogsFactory {
+    return new Groth16VerifierWithLogsFactory(o);
   }
 
-  async proofAndSignals(
+  protected async getVkey(
+    zKey: snarkjs.ZKArtifact,
+    curve: any,
+  ): Promise<GrothVerificationKey> {
+    return getGroth16Vkey(zKey, curve);
+  }
+
+  protected encodeVkey(
+    vk: GrothVerificationKey,
+    appSpec: Arc56Contract,
+  ): Uint8Array {
+    return encodeGroth16Vk(vk, appSpec);
+  }
+
+  protected encodeProof(proof: any, curve: any): GrothProof {
+    return encodeGroth16Proof(proof, curve);
+  }
+
+  protected async fullProve(
     inputs: snarkjs.CircuitSignals,
-  ): Promise<Groth16Witness> {
-    await this.ensureCurveInstanttiation();
-
-    const { proof: rawProof, publicSignals: rawSignals } =
-      await snarkjs.groth16.fullProve(inputs, this.wasmProver, this.zKey);
-
-    const proof = encodeGroth16Proof(rawProof, this.curve);
-    const signals = encodeGroth16Signals(...rawSignals);
-
-    return {
-      proof,
-      signals,
-    };
-  }
-
-  private assertDeployed(): asserts this is {
-    appClient: Groth16VerifierClient;
-  } {
-    if (!this.appClient) {
-      throw new Error("AppVerifier not deployed");
-    }
-  }
-
-  // Methods that take in proof and signals directly
-
-  async simulateVerificationWithProofAndSignals(
-    proofAndSignals: { proof: GrothProof; signals: bigint[] },
-    simParams?: RawSimulateOptions,
-  ) {
-    this.assertDeployed();
-
-    return this.appClient
-      .newGroup()
-      .verify({ args: proofAndSignals })
-      .simulate(simParams ?? {});
-  }
-
-  async verifyTransactionFromProofAndSignals(
-    proofAndSignals: Groth16Witness,
-  ): Promise<Transaction> {
-    this.assertDeployed();
-
-    return (
-      await this.appClient.createTransaction.verify({
-        args: proofAndSignals,
-      })
-    ).transactions[0]!;
-  }
-
-  async callVerifyFromProofAndSignals(
-    proofAndSignals: Groth16Witness,
-    callParams?: Omit<
-      AppClientMethodCallParams,
-      "method" | "args" | "onComplete"
-    >,
-  ) {
-    this.assertDeployed();
-
-    return this.appClient.send.verify({ ...callParams, args: proofAndSignals });
-  }
-
-  // Methods that generate proof and signals internally
-
-  async simulateVerification(
-    inputs: snarkjs.CircuitSignals,
-    simParams?: RawSimulateOptions,
-  ) {
-    return this.simulateVerificationWithProofAndSignals(
-      await this.proofAndSignals(inputs),
-      simParams,
-    );
-  }
-
-  async verifyTransaction(
-    inputs: snarkjs.CircuitSignals,
-  ): Promise<Transaction> {
-    return this.verifyTransactionFromProofAndSignals(
-      await this.proofAndSignals(inputs),
-    );
-  }
-
-  async callVerify(
-    inputs: snarkjs.CircuitSignals,
-    callParams?: Omit<
-      AppClientMethodCallParams,
-      "method" | "args" | "onComplete"
-    >,
-  ) {
-    return this.callVerifyFromProofAndSignals(
-      await this.proofAndSignals(inputs),
-      callParams,
-    );
+    wasmProver: snarkjs.ZKArtifact,
+    zKey: snarkjs.ZKArtifact,
+  ): Promise<{ proof: any; publicSignals: any }> {
+    return snarkjs.groth16.fullProve(inputs, wasmProver, zKey);
   }
 }
